f1526cb969d702e18663d7bd4f9b5fba6cb6c656
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-05-17 14:51:00 +0900

    2

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -24,5 +24,13 @@ public class PlayerController : MonoBehaviour
         {
             rigitbody.AddForce(new Vector3(-1, 0, 0) * power);
         }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigitbody.AddForce(new Vector3(0, 0, -1) * power);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigitbody.AddForce(new Vector3(1, 0, 0) * power);
+        }
     }
 }
