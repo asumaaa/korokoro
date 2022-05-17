6dea577a6aca7db104d2b929ecd6629978372f97
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-05-17 13:38:13 +0900

    1回

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float power = 10;
+    public Rigidbody rigitbody;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigitbody.AddForce(new Vector3(0, 0, 1) * power);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigitbody.AddForce(new Vector3(-1, 0, 0) * power);
+        }
+    }
+}
